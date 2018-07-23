85ba05a56c331b3ebd4470fbab86e44f1ce84a8d
stepinski <stepinski@gmail.com>
2018-07-23 16:10:34 +0200

    functions

--- test.hs
+++ test.hs
@@ -10,3 +10,26 @@ n = a `div` length xs
  where
     a = 10
     xs = [1,2,3,4,5]
+
+
+--safetail [] = []
+-- safetail (_ : xs) = xs
+
+--safetail xs = if null xs then [] else tail xs
+
+--safetail xs
+--    | null xs = []
+--    | otherwise = tail xs
+
+--safetail [] = []
+--safetail xs = tail xs 
+-- safetail [] = []
+
+-- safetail [x] = [x]
+-- safetail (_ : xs) = xs
+
+safetail 
+ = \ xs ->
+    case xs of
+        [] -> []
+        (_:xs) -> xs
