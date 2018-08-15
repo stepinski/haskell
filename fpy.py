541169ac8fe9c4463409a8d425aa7c8a6a71ca5c
stepinski <stepinski@gmail.com>
2018-08-15 14:03:05 +0200

    higher order functions and partial application

--- fpy.py
+++ fpy.py
@@ -1,3 +1,4 @@
+from functools import partial
 def teststr():
     print("aaa")
 
@@ -11,4 +12,26 @@ def testprint(x):
     print(list(map(lambda num: num*num,x)))
 
 
+def prod(xs):
+    print(reduce(lambda x,y : x*y,xs))
+
+def fltminzero(xs):
+    return list(filter(lambda num: num<0,xs))
 # execfile("/home/pioters/ws/prj/haskell/fpy.py")
+
+
+def summa(xs):
+    return sum(xs)
+
+def act(func,xs):
+    return func(xs)
+
+#print(act(summa,x))
+power = lambda x,exp: x ** exp
+square = partial(power,exp=2)
+
+powers = []
+for i in range(2,50):
+    powers.append(partial(power,exp=i))
+
+
